ae84a4d2fc30afaa517e867a2142bfd7aa63171b
Death-Raider <oragimirox@gamil.com>
2022-02-25 20:05:02 +0530

    Test runs

--- plot.py
+++ plot.py
@@ -7,13 +7,22 @@ def MA(vec,sample):
     cumsum_vec = np.cumsum(np.insert(vec, 0, 0))
     ma_vec = (cumsum_vec[sample:] - cumsum_vec[:-sample]) / sample
     return ma_vec
+
+with open("logs\Info.json") as f:
+    data = json.load(f)
+
+# cost = data["cost"]
+#
+# ma_cost = MA(cost,100)
+# plt.plot(np.arange(len(ma_cost)),ma_cost)
+# plt.show()
+
 step = []
 pred = []
 true = []
 acc = []
 cost = []
-with open("logs\Info.json") as f:
-    data = json.load(f)
+
 for epoch in range(0,1):
     cost.append(np.array(data[epoch]["cost"]))
     step.append(8000*epoch+np.array(data[epoch]["step"]))
@@ -21,16 +30,16 @@ for epoch in range(0,1):
     pred.append(np.array(data[epoch]["pred"]))
     true.append(np.array(data[epoch]["true"]))
 
-cost = np.array([*cost],dtype="object").flatten()
-step = np.array([*step],dtype="object").flatten()
-acc = np.array([*acc],dtype="object").flatten()
-ma_cost = MA(cost,500)
+    cost = np.array([*cost],dtype="object").flatten()
+    step = np.array([*step],dtype="object").flatten()
+    acc = np.array([*acc],dtype="object").flatten()
+    ma_cost = MA(cost,500)
 
-plt.plot(np.arange(len(cost)),cost,label="cost")
-plt.plot(np.arange(len(ma_cost)),ma_cost,label="MA-cost")
+    plt.plot(np.arange(len(cost)),cost,label="cost")
+    plt.plot(np.arange(len(ma_cost)),ma_cost,label="MA-cost")
 
-plt.plot(step,acc,label="accuracy")
-plt.plot(step[0::997],acc[0::997],label="accuracy-peak")
+    plt.plot(step,acc,label="accuracy")
+    plt.plot(step[0::997],acc[0::997],label="accuracy-peak")
 
-plt.savefig("test")
-plt.show()
+    plt.savefig("test")
+    plt.show()
